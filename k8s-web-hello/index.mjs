238a76484205188ab7cceaf721b008eb8333fab7
Suket Shah <suket.shah@utexas.edu>
2023-01-24 08:46:47 -0600

    changing the package.json file

--- k8s-web-hello/index.mjs
+++ k8s-web-hello/index.mjs
@@ -1,26 +1,49 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import os from 'os';
+// import express from 'express';
+// import fetch from 'node-fetch';
+// import os from 'os';
+//
+// const app = express();
+// const port = 3000;
+//
+// app.get('/', (req, res) => {
+//   const helloMessage = `VERSION 2: Hello World! I'm ${os.hostname()}`;
+//   console.log(helloMessage);
+//   res.send(helloMessage);
+// });
+//
+// app.get('/nginx', async (req, res) => {
+//   // proxy between nginx and nodejs
+//   const url = 'http://nginx';
+//   const response = await fetch(url);
+//   const body = await response.text();
+//   res.send(body);
+// });
+//
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+//
+// // The code above is a simple Express.js application that returns a message with the hostname of the machine where the application is running. The application is listening on port 3000.
+import express from 'express'
+import fetch from 'node-fetch'
+import os from 'os'
 
-const app = express();
-const port = 3000;
+const app = express()
+const PORT = 3000
 
-app.get('/', (req, res) => {
-  const helloMessage = `VERSION 2: Hello World! I'm ${os.hostname()}`;
-  console.log(helloMessage);
-  res.send(helloMessage);
-});
+app.get("/", (req, res) => {
+  const helloMessage = `Hello from the ${os.hostname()}`
+  console.log(helloMessage)
+  res.send(helloMessage)
+})
 
-app.get('/nginx', async (req, res) => {
-  // proxy between nginx and nodejs
-  const url = 'http://nginx';
+app.get("/nginx", async (req, res) => {
+  const url = 'http://nginx'
   const response = await fetch(url);
   const body = await response.text();
-  res.send(body);
-});
+  res.send(body)
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// The code above is a simple Express.js application that returns a message with the hostname of the machine where the application is running. The application is listening on port 3000.
+app.listen(PORT, () => {
+  console.log(`Web server is listening at port ${PORT}`)
+})
